dbac357b72f592aa01de26f3cf7e5f1c4c5f1311
JulianCelisCreator <celisjuliandavid@gmail.com>
2025-12-20 16:46:58 -0500

    added section 6 and 7

---
+++ section_7/course/vector.cpp
@@ -0,0 +1,53 @@
+//
+// Created by juliancelis on 2025-12-20.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+    vector <int> vector1;
+    vector <int> vector2;
+
+    vector1.push_back(10);
+    vector1.push_back(20);
+
+    cout<< "First element vector 1: "<<vector1.at(0)<<endl;
+    cout<< "Second element vector 2: "<<vector1.at(1)<<endl;
+    cout<< "vector 1 elements: "<<vector1.size()<<endl;
+    cout<<endl;
+
+    vector2.push_back(100);
+    vector2.push_back(200);
+
+    cout<< "First element vector 2: "<<vector2.at(0)<<endl;
+    cout<< "First element vector 2: "<<vector2.at(1)<<endl;
+    cout<<"Size vector 2: "<<vector2.size()<<endl;
+    cout<<endl;
+    vector <vector<int>> vector_2d;
+
+    vector_2d.push_back(vector1);
+    vector_2d.push_back(vector2);
+
+    cout<<"Vector 2D"<<endl;
+    cout<<vector_2d.at(0).at(0)<< " " <<vector_2d.at(0).at(1)<<endl;
+    cout<<vector_2d.at(1).at(0)<< " " << vector_2d.at(0).at(1) <<endl;
+    cout<<endl;
+
+    vector1.at(0) = 1000;
+
+    cout<<"Vector 2D"<<endl;
+    cout<<vector_2d.at(0).at(0)<< " " <<vector_2d.at(0).at(1)<<endl;
+    cout<<vector_2d.at(1).at(0)<< " " << vector_2d.at(0).at(1) <<endl;
+    cout<<endl;
+
+    cout<< "First element vector 1: "<<vector1.at(0)<<endl;
+    cout<< "Second element vector 2: "<<vector1.at(1)<<endl;
+    cout<< "vector 1 elements: "<<vector1.size()<<endl;
+    cout<<endl;
+
+    return 0;
+}
