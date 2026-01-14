6d1fd2396d9175bf076dbb2239be8af89cef7490
JulianCelisCreator <celisjuliandavid@gmail.com>
2026-01-14 10:35:44 -0500

    added new exercises

---
+++ sections/exercises/vectors/exercise_3.cpp
@@ -0,0 +1,25 @@
+//
+// Created by juliancelis on 2026-01-14.
+//
+
+// Maximum Subarray Sum (No Brute Force)
+// Given a vector with positive and negative numbers:
+//
+// Find the contiguous subvector with the maximum sum
+//
+// Output:
+//
+// maximum sum
+//
+// starting index
+//
+// ending index
+//
+// 📌 You may not check all possible subvectors.
+//
+#include <iostream>
+#include <vector>
+
+int main () {
+
+}
