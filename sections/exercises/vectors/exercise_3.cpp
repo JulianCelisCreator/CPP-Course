5fef8903afa4813426665851df47070c0f3ec582
JulianCelisCreator <celisjuliandavid@gmail.com>
2026-02-26 21:21:09 -0500

    added exercise 3

--- sections/exercises/vectors/exercise_3.cpp
+++ sections/exercises/vectors/exercise_3.cpp
@@ -31,58 +31,77 @@
 
 using namespace std;
 
-std::vector<int> max_subArray(std::vector<int> const &v) {
-    int max_sum {v[0]};
-    int start_index {0};
-    int end_index {0};
-    int current_sum{0};
+struct resultado {
+    int max_sum;
+    int start_index;
+    int end_index;
+};
+
+resultado max_subArray(std::vector<int> const &v) {
+    resultado r;
+    int current_sum{v[0]};
     int temp_start{0};
+    r.start_index = 0;
+    r.end_index = 0;
+    r.max_sum = v[0];
 
-    std::vector<int> temporal;
-    for (size_t i= 0; i<v.size();i++ ) {
-        temporal.push_back(v[i]);
-
-        for (size_t j= temp_start; j<temporal.size();j++ ) {
-
-            current_sum += temporal[j];
+    for (size_t i = 1; i < v.size(); i++) {
+        if (current_sum + v[i] < v[i]) {
+            current_sum = v[i];
+            temp_start = i;
+        } else {
+            current_sum += v[i];
         }
-        cout<<current_sum<<endl;
-        if (current_sum > 0) {
-            if (current_sum>max_sum) {
-                max_sum = current_sum;
-                cout<<max_sum<<endl;
-                temp_start = i;
-            }
-        }
-        else {
-            temporal.clear();
-            current_sum = 0;
+        if (current_sum > r.max_sum) {
+            r.max_sum = current_sum;
+            r.start_index = temp_start;
+            r.end_index = i;
         }
     }
-    return temporal;
-    }
+    return r;
+}
 
 
-
-
-int main () {
-
-    std::vector<std::vector<int>> testCases = {
-        {-2, 1, -3, 4, -1, 2, 1, -5, 4}
+int main() {
+    resultado r;
+    std::vector<std::vector<int> > testCases = {
+        {-2, 1, -3, 4, -1, 2, 1, -5, 4},
+        {1, 2, 3, 4},
+        {-8, -3, -6, -2, -5, -4},
+        {5, 4, -1, -2, -3},
+        {-2, -1, 3, 4},
+        {1, -1, 1, -1, 1}
     };
 
-    std::vector<std::vector<int>> solution = {
-        {4,-1,2,1}
+    std::vector<std::vector<int> > solution = {
+        {4, -1, 2, 1},
+        {1, 2, 3, 4},
+        {-2},
+        {5, 4},
+        {3, 4},
+        {1}
     };
 
-    for (size_t i=0; i < testCases.size();i++) {
+    for (size_t i = 0; i < testCases.size(); i++) {
+        resultado r1{};
+
         std::vector<int> input = testCases[i];
-        std::vector<int> correction =  max_subArray(input);
-        if (solution[i] != correction) {
-            cout<<"The result are different";
+        r1 = max_subArray(input);
+        cout << i << ". " << " Solución" << endl;;
+        cout << "Indice inicio: " << r1.start_index << endl;
+        cout << "Indice final: " << r1.end_index << endl;
+        cout << "Suma máxima: " << r1.max_sum << endl;
+
+
+        std::vector<int> subarray;
+        for (size_t j = r1.start_index; j <= r1.end_index; j++) {
+            subarray.push_back(input[j]);
         }
-        else {
-            cout<<"The result are expected";
+        if (solution[i] != subarray) {
+            cout << "The result are different" << endl;
+        } else {
+            cout << "The result is expected" << endl;
         }
     }
+    return 0;
 }
